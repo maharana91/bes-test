b6217ed4a7911e926df691560672a4b7d294c533
maharana91 <maharana.pratap@pravisht.com>
2024-06-14 16:45:36 +0530

    code resized

--- complete/src/main/java/com/example/consumingwebservice/controller/BesController.java
+++ complete/src/main/java/com/example/consumingwebservice/controller/BesController.java
@@ -3,12 +3,16 @@ package com.example.consumingwebservice.controller;
 //import com.example.consumingwebservice.CountryClient;
 //import com.example.consumingwebservice.stubs.Currency;
 //import com.example.consumingwebservice.stubs.GetCountryResponse;
+
+import com.example.consumingwebservice.BESClient;
+import com.example.consumingwebservice.dto.BesDTO;
+import com.example.consumingwebservice.entity.BesEntity;
+import com.example.consumingwebservice.mapper.BesMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.consumingwebservice.BESClient;
 
 
 @RestController
@@ -16,9 +20,17 @@ import com.example.consumingwebservice.BESClient;
 public class BesController {
 
     @Autowired
+    private BesMapper mapper;
+
+    @Autowired
     private BESClient besClient;
     @GetMapping("/test")
     public String test() {
+        BesEntity entity = new BesEntity();
+        entity.setMsg("entity msg value");
+
+        BesDTO dto = mapper.entityToDto(entity);
+
         return "Controller-Test";
     }
 
